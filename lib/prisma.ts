300d28e0ef83f33fa618ddaea95e83cea5e871b9
rk-1996 <rajkamdiya@gmail.com>
2021-08-25 20:52:55 +0530

    changes in lib/prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,10 +5,10 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+  // if (!global.prisma) {
+  //   global.prisma = new PrismaClient();
+  // }
+  // prisma = global.prisma;
 }
 
 export default prisma;
